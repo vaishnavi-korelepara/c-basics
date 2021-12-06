fd47ab56434fd32858fd8ef9272f88b948bee733
vaishnavi-korelepara <93148663+vaishnavi-korelepara@users.noreply.github.com>
2021-12-06 12:16:51 +0530

    Add files via upload

---
+++ number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int no1,no2;
+	scanf("%d%d",&no1,&no2);
+	if(no1>no2)
+	{
+	
+	  printf("%d is bigger", no1);
+}
+	else
+	{
+		printf("%d is bigger", no2);
+	}
+}	
