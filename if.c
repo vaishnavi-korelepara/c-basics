ab853b13d83783598f98e2c2f8dece2f6a5dda86
vaishnavi-korelepara <93148663+vaishnavi-korelepara@users.noreply.github.com>
2021-12-06 12:17:53 +0530

    Add files via upload

---
+++ if.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int age;
+	scanf("%d",&age);
+	if(age>=18)
+    {
+    		printf("eligible for vote");
+	}
+else
+    {
+    		printf("not eligible for vote");
+}
+}
