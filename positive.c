81301d2e39c51b8f564af218baef7abe4e85231b
vaishnavi-korelepara <93148663+vaishnavi-korelepara@users.noreply.github.com>
2021-12-06 12:16:12 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+ {
+ 	int num;
+ 	scanf("%d", &num);
+ 	if(num>0)
+ 	{
+	 
+ 		printf("%d num is positive", num);
+}
+ 	else
+ 	{
+ 		printf("%d num is negative", num);
+	 }
+
+}
+ 
