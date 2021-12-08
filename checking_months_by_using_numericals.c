1f94de26439caf68cdc9e7cb8f122b740d043e4c
vaishnavi-korelepara <93148663+vaishnavi-korelepara@users.noreply.github.com>
2021-12-08 21:42:33 +0530

    Add files via upload

---
+++ checking_months_by_using_numericals.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+void main()
+{
+	int number;
+	printf("enter numbers :");
+	scanf("%d", &number);
+	if(number==1)
+	{
+		printf("%d month is january",number);
+	}
+	else if(number==2)
+	{
+		printf("%d  month is febraury",number);
+	}
+	else if(number==3)
+	{
+		printf("%d month is march",number);
+	}
+	else if(number==4)
+	{
+		printf("%d month is april",number);
+	}
+	else if(number==5)
+	{
+		printf("%d monthis may",number);
+	}
+	else if(number==6)
+	{
+		printf("%d month is june",number);
+		
+	}
+	else if(number==7)
+	{
+		printf("%d month is july",number);
+	}
+	else if(number==8)
+	{
+		printf("%d  month is august",number);
+	}
+	else if(number==9)
+	{
+		printf("%d month is september",number);
+	}
+	else if(number==10)
+	{
+		printf("%d  month is october",number);
+	}
+	else if(number==11)
+	{
+		printf("%d  month is november",number);
+	}
+	else if(number==1)
+	{
+		printf("%d month is december",number);
+	}
+}
